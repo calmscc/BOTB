4efa4d49f8083269a30c76f616766fbbb4cc9fde
Caleb Maxie <cam71805@gmail.com>
2026-03-13 05:28:25 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ SERVE FRONTEND
 ========================================
 */
 
-app.use(express.static(path.join(__dirname,"frontend")))
+app.use(express.static(path.join(__dirname,"../frontend")))
 
 app.get("/",(req,res)=>{
  res.sendFile(path.join(__dirname,"frontend/index.html"))
